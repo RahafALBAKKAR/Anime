a67895ec287506260d8e0dcdecdbda4361b4c7f7
rahafalbakkar <rahaf.a7med1991@gmail.com>
2021-08-06 18:02:08 +0300

    add new

---
+++ app.js
@@ -0,0 +1,14 @@
+let program=prompt('How your skills in programing!!','very good,good,not bad');
+console.log(program);
+
+
+let htmlStand=prompt('What is HTML standing for?');
+console.log(htmlStand);
+
+let cssStand=prompt('What is CSS standing for!!')
+console.log(cssStand);
+
+let rate=prompt('how many do you rate yourself for ready to learn?')
+console.log(rate);
+alert('MY programing skills is ' + program + 'so lets start in HTML is stand for'
+ + htmlStand + 'and CSS' + cssStand+ 'this user ready to learn and rate himself' + rate);
--- testt.js
+++ testt.js
@@ -1,4 +1,44 @@
-joko
-ihiuwhduwb
-var x = ((2 * 90) === 180) && (false && 'true')
-console.log(x)
+let Profiles = document.getElementById('salmone');
+
+const Seattle =  {
+  name: 'Seattle',
+  min:23 ,
+
+  max:65,
+
+  avg:6.3,
+
+  cookies: 0,
+  total: 0,
+  hour:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
+  randomFun: function(){
+    this.cookies = Math.floor(Math.random() * (( this.max - this.min + 1) + this.min));
+    this.cookies = Math.floor(this.cookies * this.avg) ;
+    this.total = this.total + this.cookies ;
+    return this.cookies;
+  
+  },
+  land: function(){
+    let articleElement = document.createElement('article');
+    Profiles.appendChild(articleElement);
+
+    let h2Element = document.createElement('h2');
+    h2Element.textContent = this.name;
+    articleElement.appendChild(h2Element);
+
+    let ulElement = document.createElement('ul');
+    articleElement.appendChild(ulElement);
+
+    for(let i = 0; i <this.hour.length ; i++) {
+      let liElement = document.createElement('li');
+      liElement.textContent = this.hour[i] + ' ' + this.randomFun() + '  ' + 'cookies';
+      ulElement.appendChild(liElement);
+    }
+    let liElement = document.createElement('li');
+    liElement.textContent = 'Total:' + '     ' + this.total + '  ' + 'cookies';
+    ulElement.appendChild(liElement);
+  }
+
+};
+
+Seattle.land();
